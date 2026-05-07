22de6ede6e27e9197344406b096f2e8143d4c8b4
Marffe <efframlol@gmail.com>
2026-05-07 11:42:46 -0500

    Spanish (es_419) translation

---
+++ localization/es_419.lua
@@ -0,0 +1,194 @@
+return {
+    descriptions = {
+        Back = {
+            b_broo_brook={
+                name="Baraja Brook",
+                text={
+                    "Los {C:attention}Comodines{} {C:blue}Comunes{}",
+                    "aparecen con la mitad de frecuencia",
+                },
+            },
+        },
+        Joker={
+            j_broo_stargaze={
+                name = 'Contempla las Estrellas',
+                text = {
+                    "Vende esta carta para subir",
+                    "{C:planet}#2#{} niveles a {C:attention}#1#{}",
+                    "el tipo de mano de póker cambia",
+                    "después de cada jugada",
+                },
+            },
+            j_broo_d4c={
+                name = 'D4C',
+                text = {
+                    "Añade edición {C:dark_edition}Laminada{} a un",
+                    "{C:attention}Comodín{} al azar al final de la tienda",
+                    "No ganas {C:attention}Interés{}",
+                },
+            },
+            j_broo_ink={
+                name = 'Tinta de Imprenta',
+                text = {
+                    "Cuando se selecciona una {C:attention}Ciega{},",
+                    "crea una carta {C:tarot}Tarot{} que",
+                    "coincida con el {C:attention}palo más común{}",
+                    "en tu baraja completa",
+                    "{C:inactive}(Debe haber espacio)",
+                },
+            },
+            j_broo_occultist={
+                name = 'Ocultista',
+                text = {
+                    "Intercambia la frecuencia de aparición",
+                    "de los {C:attention}Paquetes Estándar{} y",
+                    "los {C:spectral}Paquetes Espectrales{} en la tienda",
+                },
+            },
+            j_broo_trace={
+                name = 'Rastro',
+                text = {
+                    "Prob. de {C:green}#1# en #2#{} de ganar",
+                    "{C:money}$#3#{} al final de la ronda",
+                    "{S:1.1,C:red,E:2}se autodestruye",
+                },
+            },
+            j_broo_cattail={
+                name = 'Totora',
+                text = {
+                    "{C:red}Destruye{} las siguientes",
+                    "{C:attention}#1#{} cartas que anoten",
+                },
+            },
+            j_broo_yeast={
+                name = 'Levadura',
+                text = {
+                    "Gana {X:mult,C:white}X#2#{} Multi",
+                    "al final de la ronda, la cantidad",
+                    "aumenta en {X:mult,C:white}X#3#{}",
+                    "cuando derrotas una {C:attention}Ciega Jefe{}",
+                    "{C:inactive}(Actualmente {X:mult,C:white}X#1#{C:inactive} Multi)",
+                },
+            },
+            j_broo_unease={
+                name = 'Inquietud',
+                text = {
+                    "{C:mult}+#1#{} Multi Base",
+                },
+            },
+            j_broo_alien={ -- Esto no se usa en ningún lado pero el codigo del comodin existe
+                name = 'Extraterrestre',
+                text = {
+                    "Cada tienda tiene",
+                },
+            },
+            j_broo_needle_thread={
+                name = 'Aguja e Hilo',
+                text = {
+                    "Redondea las {C:blue}Fichas{}",
+                    "hasta el siguiente centenar",
+                    "{C:inactive}(ej: 25 -> 100)",
+                },
+            },
+            j_broo_dancer={
+                name = 'Bailarina',
+                text = {
+                    "Reactiva todas las",
+                    "{C:attention}Cartas de Juego{}",
+                    "si las ranuras de Comodines",
+                    "no están llenas",
+                },
+            },
+            j_broo_pulp_fiction={
+                name = 'Pulp Fiction',
+                text = {
+                    "Los {C:attention}2s{}, {C:attention}3s{}, {C:attention}4s{} o {C:attention}5s{} descartados",
+                    "ya no regresan a la {C:attention}baraja{}",
+                    "hasta que esta carta",
+                    "se {C:money}venda{} o {C:red}destruya{}",
+                },
+            },
+            j_broo_parrot={
+                name = 'Loro',
+                text = {
+                    "Crea una {C:attention}Etiqueta de Vale{}",
+                    "al final de la ronda",
+                },
+            },
+            j_broo_moon_rabbit={
+                name = 'Conejo Lunar',
+                text = {
+                    "Gana {X:mult,C:white}X#4#{} Multi",
+                    "por cada {C:money}$#1#{C:inactive}[#2#]{} de",
+                    "{C:attention}interés{} ganado",
+                    "{C:inactive}(Actualmente {X:mult,C:white}X#3#{C:inactive} Multi)",
+                },
+            },
+            j_broo_baby_dinosaur={
+                name = 'Bebé Dinosaurio',
+                text = {
+                    "{C:attention}-#1#{} Apuesta al final de la",
+                    "{C:attention}Ciega Pequeña{} o {C:attention}Ciega Grande{}",
+                    "{S:1.1,C:red,E:2}se autodestruye{}",
+                },
+            },
+        },
+        Mod={
+            Brook={
+                name = "Brook",
+                text = {
+                    "Añade {C:red}15{} Comodines Vanilla",
+                    "y {C:purple}1{} Baraja",
+                    " ",
+                    "{C:dark_edition}Concepto & Arte{}",
+                    "Brookling",
+                    " ",
+                    "{C:gold}Código{}",
+                    "Baimao",
+                    " ",
+                    "{C:green}Gracias a{}",
+                    "Snowlight, mleaf233,",
+                    "280chan, Seastar y 22461",
+                    " ",
+                    "Haz clic para ver la página del {C:blue}wiki{}:",
+                    "{C:attention}https://balatromods.miraheze.org/wiki/Brook{}",
+                }
+            },
+            About={
+                name = "Acerca de",
+                text = {
+                    "¡Hola amigos! Soy Brookling.",
+                    " ",
+                    "Desde el lanzamiento de Balatro en {C:attention}Febrero 2024{}, he estado jugando a este juego y",
+                    "realmente me encanta. Estos días me enfoco principalmente en partidas en el {C:money}Pozo dorado{},",
+                    "con un récord personal de {C:red}18{}.",
+                    " ",
+                    "{C:attention}Desde Febrero hasta Agosto 2025{}, me dediqué a crear un mod",
+                    "{C:blue}de alta calidad{} y {C:blue}estilo vanilla{},",
+                    "inspirándome mucho en muchos mods existentes y agradeciendo profundamente a sus creadores.",
+                    " ",
+                    "Diseñé {C:red}15{} Comodines nuevos, cada uno con habilidades únicas en una amplia",
+                    "variedad de tipos, poniendo mucho esfuerzo en el equilibrio, la diversión y la innovación,",
+                    "e ilustrando todas las cartas yo mismo.",
+                    " ",
+                    "{C:green}Agradecimientos especiales{} a Baimao por programar este proyecto, y a todos los amigos",
+                    "que lo apoyaron. {C:dark_edition}Brook{} ya está lanzado y listo para jugar.",
+                }
+            },
+        },
+    },
+    misc={
+        dictionary={
+            b_open_brook_wiki="Abrir en Navegador",
+            b_brookling_about="Acerca de",
+            b_brook_github="GitHub",
+            b_brook_discord="Discord",
+            k_d4c="¡Barato!",
+            k_free_reroll_1="gratis",
+            k_free_reroll_2="Reintentos",
+            k_plus_tag="+1 Etiqueta",
+            k_burnt_out="¡Quemado!",
+            k_rewind="¡Rebobinar!",
+        },
+    },
+}
